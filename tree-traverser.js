c4783aa57248b4a1f63aab3d7c561a08881d1c1f
liyan <yanx0167@stud.kea.dk>
2018-09-11 16:38:33 +0200

    version 4 finished: put all into 1 string including indentation

--- tree-traverser.js
+++ tree-traverser.js
@@ -1,24 +1,21 @@
 "use strict";
 
-// Version 1 just writes the name and attributes of each HTML-element. The output looks somewhat like this:
 function getAllAttr(elem) {
   if (elem.attributes.length > 0) {
     for (let i = 0; i < elem.attributes.length; i++) {
       /////// element.attribute returns a map, not an array, can't use arr.forEach
       /////// console.log(typeof doc.attributes[i]); // returns object, can't get value with just index number, need to use key to acess value
-      console.log(
-        `${indent.repeat(levelCount)}<${elem.tagName.toLowerCase()} ${
-          elem.attributes[0].nodeName
-        }="${elem.attributes[0].nodeValue}">`
-      );
+      htmlString += `
+${indent.repeat(levelCount)}<${elem.tagName.toLowerCase()} ${
+        elem.attributes[0].nodeName
+      }="${elem.attributes[0].nodeValue}">`;
     }
   } else {
-    console.log(`${indent.repeat(levelCount)}<${elem.tagName.toLowerCase()}>`);
+    htmlString += `
+${indent.repeat(levelCount)}<${elem.tagName.toLowerCase()}>`;
   }
 }
 
-let levelCount = 0;
-let indent = "  ";
 function traverse(parent) {
   getAllAttr(parent);
   if (parent.children.length > 0) {
@@ -33,10 +30,12 @@ function traverse(parent) {
 }
 
 // starts from here
+let levelCount = 0;
+let indent = "  ";
+let htmlString = "";
 window.addEventListener("DOMContentLoaded", init);
 function init() {
   const baseHTML = document.documentElement;
   traverse(baseHTML);
+  console.log(htmlString);
 }
-
-// version 2
