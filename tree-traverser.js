b88be976c962ceac56e3edc79ddb1318abd1cd06
liyan <yanx0167@stud.kea.dk>
2018-09-11 16:25:11 +0200

    version 3 finished: indentation

--- tree-traverser.js
+++ tree-traverser.js
@@ -7,25 +7,32 @@ function getAllAttr(elem) {
       /////// element.attribute returns a map, not an array, can't use arr.forEach
       /////// console.log(typeof doc.attributes[i]); // returns object, can't get value with just index number, need to use key to acess value
       console.log(
-        `<${elem.tagName.toLowerCase()} ${elem.attributes[0].nodeName}="${
-          elem.attributes[0].nodeValue
-        }">`
+        `${indent.repeat(levelCount)}<${elem.tagName.toLowerCase()} ${
+          elem.attributes[0].nodeName
+        }="${elem.attributes[0].nodeValue}">`
       );
     }
   } else {
-    console.log(`<${elem.tagName.toLowerCase()}>`);
+    console.log(`${indent.repeat(levelCount)}<${elem.tagName.toLowerCase()}>`);
   }
 }
 
+let levelCount = 0;
+let indent = "  ";
 function traverse(parent) {
   getAllAttr(parent);
   if (parent.children.length > 0) {
+    levelCount++;
     for (let i = 0; i < parent.children.length; i++) {
       traverse(parent.children[i]);
+    }
+    if (levelCount >= 1) {
+      levelCount--;
     }
   }
 }
 
+// starts from here
 window.addEventListener("DOMContentLoaded", init);
 function init() {
   const baseHTML = document.documentElement;
