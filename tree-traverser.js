4ca3098bcb3b44ceda76d18b9e4ed1bfa6bb14eb
liyan <yanx0167@stud.kea.dk>
2018-09-11 15:28:19 +0200

    cleared up js

--- tree-traverser.js
+++ tree-traverser.js
@@ -5,14 +5,12 @@ function getAllAttr(elem) {
   if (elem.attributes.length > 0) {
     for (let i = 0; i < elem.attributes.length; i++) {
       /////// element.attribute returns a map, not an array, can't use arr.forEach
-      /////// console.log(typeof doc.attributes[i]); // returns object, can't get value with just [], need to use key to acess value
-
+      /////// console.log(typeof doc.attributes[i]); // returns object, can't get value with just index number, need to use key to acess value
       console.log(
         `${elem.tagName.toLowerCase()}: ${elem.attributes[0].nodeName}="${
           elem.attributes[0].nodeValue
         }"`
       );
-      //    console.log(doc.attributes);
     }
   } else {
     console.log(`${elem.tagName.toLowerCase()}`);
