5c051694d245ff7640a0d0a8e8239e0bfab73553
liyan <yanx0167@stud.kea.dk>
2018-09-12 10:43:50 +0200

    gap count working, index level still not right

--- tree-traverser_node.js
+++ tree-traverser_node.js
@@ -3,12 +3,26 @@
 function getTag(elem) {
   //  console.log(elem);
   if (elem.childNodes[0]) {
+    textLevel++;
+    gapCount = 0;
     for (let ii = 0; ii < elem.childNodes.length; ii = ii + 2) {
       if (elem.childNodes[ii].nodeValue.replace(/\s+/g, "").length > 0) {
+        // has sub-text
         let textPiece = elem.childNodes[ii].nodeValue
           .replace(/\s+/g, " ")
           .trim();
-        console.log(`${textPiece} (text index: ${ii})`);
+        //        console.log(elem.childNodes[ii].nodeValue + "/");
+        if (
+          elem.childNodes[ii].nodeValue[
+            elem.childNodes[ii].nodeValue.length - 1
+          ] === " "
+        ) {
+          gapCount++;
+        }
+        console.log(
+          `${textPiece} (text index: ${textLevel} ${ii}) 
+gap count: ${gapCount}`
+        );
       }
     }
   }
@@ -34,13 +48,13 @@ function getTag(elem) {
 function traverseNode(parent) {
   getTag(parent);
   if (parent.childNodes.length > 1) {
-    levelCount++;
+    indentLevelCount++;
     for (let i = 1; i < parent.childNodes.length; i = i + 2) {
       traverseNode(parent.childNodes[i]);
     }
-    //     if (levelCount >= 1) {
-    //       levelCount--;
-    //     }
+    // if (indentLevelCount >= 1) {
+    //   indentLevelCount--;
+    // }
     //     if (!noClosingTagsArray.includes(`${parent.tagName.toLowerCase()}`))
     //       htmlString += `
     // ${indent.repeat(levelCount)}</${parent.tagName.toLowerCase()}>`;
@@ -52,8 +66,10 @@ function traverseNode(parent) {
 }
 
 // starts from here
-let levelCount = 0;
+let indentLevelCount = 0;
 let indent = "  ";
+let gapCount;
+let textLevel = 0;
 let htmlString = "";
 let noClosingTagsArray = [
   "area",
