54c45596d6367caffa59d62d824c058187d83fd9
liyan <yanx0167@stud.kea.dk>
2018-09-12 19:53:51 +0200

    cleared out code for easy readig  in console + add indents

--- tree-traverser_node.js
+++ tree-traverser_node.js
@@ -1,18 +1,17 @@
 "use strict";
 
 function getTag(elem) {
-  //  console.log(elem);
   if (elem.childNodes[0]) {
     textLevel = 0;
-    console.log("<" + elem.localName + ">");
+    //    console.log("<" + elem.localName + ">");
     htmlString += `
-<${elem.localName}>`;
+${indent.repeat(indentLevelCount)}<${elem.localName}>`;
     for (let ii = 0; ii < elem.childNodes.length; ii = ii + 2) {
       if (
         elem.childNodes[ii].nodeValue &&
         elem.childNodes[ii].nodeValue.replace(/\s+/g, "").length > 0
       ) {
-        // has sub-text
+        // only for those that have sub-text and sub-text is not ''
         let textPiece = elem.childNodes[ii].nodeValue
           .replace(/\s+/g, " ")
           .trim();
@@ -25,20 +24,21 @@ function getTag(elem) {
           textLevel++;
           gapCount++;
         }
-        console.log(`${textPiece}`);
-        htmlString += `${textPiece}`;
+        //        console.log(`${textPiece}`);
+        htmlString += `${textPiece}
+`;
 
-        let textPieceData = [textPiece, textLevel, ii, gapCount, ""];
+        let textPieceData = [textPiece, textLevel, ii, gapCount];
 
         textPieceArray.push(textPieceData);
         // check if gap count increase
-        if (
-          textPieceArray.length > 1 &&
-          textPieceArray[textPieceArray.length - 1][3] >
-            textPieceArray[textPieceArray.length - 2][3]
-        ) {
-          console.table(textPieceArray);
-        }
+        // if (
+        //   textPieceArray.length > 1 &&
+        //   textPieceArray[textPieceArray.length - 1][3] >
+        //     textPieceArray[textPieceArray.length - 2][3]
+        // ) {
+        //console.table(textPieceArray);
+        // }
       }
     }
   }
@@ -51,9 +51,12 @@ function traverseNode(parent) {
     for (let i = 1; i < parent.childNodes.length; i = i + 2) {
       traverseNode(parent.childNodes[i]);
     }
+    if (indentLevelCount >= 1) {
+      indentLevelCount--;
+    }
   }
-  console.log(`</${parent.localName}>`);
-  htmlString += `</${parent.localName}>
+  //  console.log(`</${parent.localName}>`);
+  htmlString += `${indent.repeat(indentLevelCount)}</${parent.localName}>
 `;
 }
 let fillCount = 0;
@@ -61,6 +64,7 @@ let fromIndex = 1;
 function reorder() {
   let gapIndexArray = [];
   textPieceArray.forEach(checkIfGap);
+
   function checkIfGap(entry, gapIndex) {
     if (
       gapIndex > 1 &&
@@ -70,16 +74,12 @@ function reorder() {
       gapIndexArray.push(gapIndex);
     }
   }
-  console.log(gapIndexArray);
+  console.table(textPieceArray);
+
   gapIndexArray.forEach(getEachIndex);
   function getEachIndex(a, indexI) {
     fillGap(a + fillCount, indexI);
   }
-  // fillGap(6);
-  // fillGap(9);
-  // fillGap(11);
-  // fillGap(19);
-  // fillGap(31);
   function fillGap(g, indexI) {
     findOne: for (let i = g + 1; i < g + 30; i++) {
       // from the index of the gap and look forward
@@ -91,20 +91,27 @@ function reorder() {
         i > fromIndex
       ) {
         // the to-be-filled texts all have [0,0,*]
-        textPieceArray.splice(g + 1, 0, ["", textPieceArray[i][0], "", "", ""]); // insert the gap text to place
-        textPieceArray[i + 1][4] = true;
-        //textPieceArray.splice(i + 1, 1); // remove the just moved
+        textPieceArray.splice(g + 1, 0, ["", textPieceArray[i][0], "", ""]); // insert the gap text to place
+        //textPieceArray[i + 1][4] = true;
         fillCount++;
         fromIndex = i + 1;
         break findOne;
       }
     }
   }
-  console.table(textPieceArray);
+  //  console.table(textPieceArray);
   console.table(
-    textPieceArray.filter(item => {
-      if (item[4] !== true) return true;
-    })
+    textPieceArray
+      .filter(item => {
+        if (item[4] !== true) return true;
+      })
+      .map(
+        item =>
+          (item = [
+            item[0].length > 1 ? item[0] : null,
+            typeof item[1] !== "number" && item[1].length > 1 ? item[1] : null
+          ])
+      )
   );
 }
 
@@ -138,5 +145,6 @@ const baseHTML = document.documentElement.childNodes[2];
 window.addEventListener("DOMContentLoaded", init);
 function init() {
   traverseNode(baseHTML);
+  console.log(htmlString);
   reorder();
 }
